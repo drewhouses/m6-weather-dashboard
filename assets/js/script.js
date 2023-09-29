dca72e1b3034719793c63e4c53f7f90a4785f018
drewhouses <drewcasas@live.com>
2023-09-29 12:43:27 -0500

    Further working on returning coordinates from cityToCoordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 var buttonEl = $(".btn");
 var cityFormEl = $("#city");
 var submitFormEl = $("#submit-form");
+var apiKey = "0a63de479cdec3863df7cb1f13d35a15";
+var coordinates = [];
 
 submitFormEl.on("submit", function (event) {
   event.preventDefault();
 
   var city = cityFormEl.val();
   console.log(`${city} is the city \nSubmitted`);
-  cityToCoordinates(city);
+  fetchWeather(cityToCoordinates(city));
 });
 
-// function fetchWeather() {
-//   fetch();
-// }
-
-function cityToCoordinates(city) {
+function fetchWeather(coords) {
+  console.log(typeof coords);
+  var lat = coords[0];
+  var lon = coords[1];
   fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
   )
     .then(function (response) {
       return response.json();
@@ -26,4 +27,16 @@ function cityToCoordinates(city) {
     });
 }
 
-//
+function cityToCoordinates(city) {
+  var xy = [];
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      coordinates = [data[0].lat, data[0].lon];
+    });
+  return coordinates;
+}
