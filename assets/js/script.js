2ef480e5111ed35fae1934ed01c24a0e78bd97c4
dhouses <drewcasas@live.com>
2023-10-03 00:03:37 -0500

    Got the saveHistory and loadHistory functions working properly on click, started displayWeather functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+var apiKey = "0a63de479cdec3863df7cb1f13d35a15";
 var buttonEl = $(".btn");
 var submitFormEl = $("#submit-form");
 var historyListEl = $(".history-sidebar");
-var apiKey = "0a63de479cdec3863df7cb1f13d35a15";
+
+loadHistory();
 
 submitFormEl.on("submit", function (event) {
   event.preventDefault();
@@ -26,24 +28,40 @@ function fetchWeather(lat, lon) {
       return response.json();
     })
     .then(function (data) {
+      console.log(data);
       displayCurrentWeather(data);
     });
 }
 
 function displayCurrentWeather(data) {
-  console.log("in my display current weather");
-  console.log(data);
-  var currentTemp = 0;
-  var currentHumidity = 0;
-  var currentHigh = 0;
-  var currentLow = 0;
+    // console.log("in my display current weather");
+    // https://openweathermap.org/weather-conditions
+  
+  var currentWeatherEl = $(".weather-container");
+  var forecastWeatherEl = $(".forecast-container");
+  var currentTemp = data.main.temp;
+  var currentHumidity = data.main.humidity;
+  var currentHigh = data.main.temp_max;
+  var currentLow = data.main.temp_min;
+
+  currentWeatherEl.append(`<section>Current temp: ${currentTemp}</section>`);
+  currentWeatherEl.append(`<section>Humidity: ${currentHumidity}</section>`);
+  currentWeatherEl.append(`<section>High: ${currentHigh}</section>`);
+  currentWeatherEl.append(`<section>Low: ${currentLow}</section>`);
+  currentWeatherEl.css('border', 'solid 1px')
 }
+
+function displayForecast() {
+  // Do the same as displayCurrentWeather just with the forecast data
+  // Need to first see how forecast data comes back from API call
+  // Then I can determine how to extract and display it
+}
+
 function cityToCoordinates(city) {
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
   )
     .then(function (response) {
-      console.log("Response to City coordinates: ", response);
       return response.json();
     })
     .then(function (data) {
@@ -55,7 +73,6 @@ function cityToCoordinates(city) {
       }
       var latitutde = data[0].lat;
       var longitude = data[0].lon;
-      console.log("This my city: ", city);
       saveToHistory(city);
       fetchWeather(latitutde, longitude);
     });
@@ -66,14 +83,15 @@ function saveToHistory(newCity) {
   if (cityHistory === null) {
     console.log("It was null");
     localStorage.setItem("searched-cities", JSON.stringify([newCity]));
+    cityHistory = JSON.parse(localStorage.getItem("searched-cities"));
   } else if (cityHistory.includes(newCity)) {
-    //do all of the checks with cityHistory as an array
     return;
   } else {
+    cityHistory.push(newCity);
   }
-  cityHistory.push(newCity);
+  // assigning updated array to local storage
   localStorage.setItem("searched-cities", JSON.stringify(cityHistory));
-
+  // creates entry in history list
   historyListEl.append(`<section>${newCity}</section>`);
 }
 
